0575f8c8d6d21e23302141fde16ffcf466a5b571
asieduofeijnr@gmail.com <asieduofeijnr@gmail.com>
2023-08-07 20:58:03 -0700

    Major update for pages

---
+++ pages/Page2_Rebound_Stats.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+
+st.set_page_config(
+    page_title="MSDS_NBA_GROUP_34",
+    page_icon="🏀",
+    layout="centered",
+    initial_sidebar_state="auto",
+    menu_items=None,
+)
+
+
+st.title("Player Rebounds")
+st.video("Hamza/hamza_video.mp4", format="mp4")
