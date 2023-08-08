3b7073f444231606b6d4b9a2427104f162dad9af
asieduofeijnr@gmail.com <asieduofeijnr@gmail.com>
2023-08-07 20:58:18 -0700

    Added Images

---
+++ pages/Page4_Conclusion.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
+import streamlit as st
+
+st.set_page_config(
+    page_title="MSDS_NBA_GROUP_34",
+    page_icon="🏀",
+    layout="centered",
+    initial_sidebar_state="auto",
+    menu_items=None,
+)
+
+st.subheader("CONCLUSION AND BUSINESS FINDINGS")
---
+++ pages/page1_Player_Stats.py
@@ -0,0 +1,24 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="MSDS_NBA_GROUP_34",
+    page_icon="🏀",
+    layout="centered",
+    initial_sidebar_state="auto",
+    menu_items=None,
+)
+
+
+images = ["Eric/peric2.png", "Eric/peric1.png", "Eric/eric5.png"]
+
+current_image_index = st.session_state.get("current_image_index", 0)
+
+st.image(
+    images[current_image_index],
+    caption=f"Page {current_image_index + 1}",
+    use_column_width=True,
+)
+
+if st.button("Next Image"):
+    current_image_index = (current_image_index + 1) % len(images)
+    st.session_state.current_image_index = current_image_index
